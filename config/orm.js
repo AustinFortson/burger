7360cf8735ce74c8683f85332d6fe629e1d08f58
Austin Fortson <a.rossfortson@gmail.com>
2019-01-15 16:49:52 -0500

    initial site files

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("connections.js");
+
+var orm = {
+    selectAll: function() {
+
+    }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Super Boy');
+INSERT INTO burgers (burger_name) VALUES ('McChicken');
+INSERT INTO bugers (burger_name) VALUES ('Sloppy Joe');
